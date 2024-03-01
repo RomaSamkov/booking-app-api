ccc7054c012e3f561161d372bde06a06b9d3ed02
RomaSamkov <Romansamkov.rs@gmail.com>
2024-03-01 13:35:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.post("/places", (req, res) => {
   });
 });
 
-app.get("/places", (req, res) => {
+app.get("/user-places", (req, res) => {
   const { token } = req.cookies;
   jwt.verify(token, jwtSecret, {}, async (err, userInfo) => {
     const { id } = userInfo;
