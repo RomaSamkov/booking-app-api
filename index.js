e67d32937f4ad67160bc3d428d5349d1cc845baf
RomaSamkov <Romansamkov.rs@gmail.com>
2024-03-15 20:35:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
+
 // app.use(
 //   cors({
 //     credentials: true,
